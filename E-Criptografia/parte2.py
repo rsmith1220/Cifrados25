b19632b9f68fd22269864e16376b42fa3b1263ab
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2025-01-23 18:46:47 -0600

    parte1

---
+++ E-Criptografia/parte2.py
@@ -0,0 +1,17 @@
+""" 
+Realizar la creación de un script que permita la conversión de palabras en texto ASCII a BINARIO
+Realizar la creación de un script que permita la conversión de palabras en texto BASE64 a BINARIO
+Realizar la creación de un script que permita la conversión de BINARIO a BASE64
+Realizar la creación de un script que permita la conversión de BINARIO a ASCII
+Realizar la creación de un script que permita la conversión de BASE64 a ASCII 
+
+    Recuerda pasar por binario para poder hacer las conversiones
+
+Realizar la creación de un script que permita aplicar XOR a un BINARIO 
+Realizar la creación de un script que permita generar llaves dinámicas (utilizar ASCII)
+Realizar la creación de un script que generar un nuevo cypher en ASCII con una llave k de tamaño fijo 
+Realizar la creación de un script que generar un nuevo cypher en ASCII con una llave k de tamaño dinámico 
+"""
+
+
+
